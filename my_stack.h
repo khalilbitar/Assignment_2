11fbf1da7acd61301c2a843017315dc3957dee6f
khalilbitar <114589360+khalilbitar@users.noreply.github.com>
2022-11-01 15:44:18 +0200

    added specs

--- my_stack.h
+++ my_stack.h
@@ -7,9 +7,17 @@ class Stack
 private:
     vector<int> s;
 public:
+    //requires: nothing
+    //effects: returns true if stack is empty and false otherwise
     bool empty() const;
+    //requires: constant reference to an integer
+    //effects: pushes the integer to the top of the stack
     void push(const int&);
+    //requires: nothing
+    //effects: returns the element on the top of the stack
     int& top();
+    //requires: nothing
+    //effects: pops element recently added to stack
     void pop();
 };
 
